876e312911c187b48c35b8e896a52e462ebd1051
suvalak22 <suvalak.na.65@ubu.ac.th>
2023-06-21 20:23:10 +0700

    create first app

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Anusorn Bunteong</span>
+        </div>
+    );
+}
+
+export default Footer;
